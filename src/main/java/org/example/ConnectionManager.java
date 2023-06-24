c231e7f97d18bef3055d799b11b67f69701c0436
khanian <khanylee@gmail.com>
2023-06-24 11:24:15 +0900

    add JdbcTemplate

--- src/main/java/org/example/ConnectionManager.java
+++ src/main/java/org/example/ConnectionManager.java
@@ -3,15 +3,39 @@ package org.example;
 import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class ConnectionManager {
-    public static DataSource getDataSource() {
-        HikariDataSource hikariDataSource = new HikariDataSource();
-        hikariDataSource.setDriverClassName("org.h2.Driver");
-        hikariDataSource.setJdbcUrl("jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1");
-        hikariDataSource.setUsername("sa");
-        hikariDataSource.setPassword("");
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final String DB_URL = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+    private static final String DB_USER = "sa";
+    private static final String DB_PASSWORD = "";
+    private static final int MAX_POOL_SIZE = 40;
+    private static final DataSource ds;
 
-        return hikariDataSource;
+    static  {
+        HikariDataSource hikariDataSource = new HikariDataSource();
+        hikariDataSource.setDriverClassName(DB_DRIVER);
+        hikariDataSource.setJdbcUrl(DB_URL);
+        hikariDataSource.setUsername(DB_USER);
+        hikariDataSource.setPassword(DB_PASSWORD);
+        hikariDataSource.setMaximumPoolSize(MAX_POOL_SIZE);
+        hikariDataSource.setMinimumIdle(MAX_POOL_SIZE);
+
+        ds = hikariDataSource;
+    }
+
+    static Connection getConnection() {
+        try {
+            return ds.getConnection();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+
+    }
+
+    static DataSource getDataSource() {
+        return ds;
     }
 }
